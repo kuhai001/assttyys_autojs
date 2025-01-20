4ba38095ba08f8153e5ce28cbeb430318b79a959
daxioanglaowang@qq.com <daxionglaowang@qq.com>
2025-01-20 16:43:19 +0800

    515

--- src/common/schemeList.ts
+++ src/common/schemeList.ts
@@ -1258,7 +1258,6 @@ const SchemeList: IScheme[] = [
 	{
 		id: 515,
 		schemeName: '日常_逢魔日常',
-		star: true,
 		list: [509, 510, 511, 40, 0, 1, 2, 3, 23, 24, 26, 508, 8, 504],
 		config: {
 			'40': {
@@ -1273,6 +1272,7 @@ const SchemeList: IScheme[] = [
 				switch_ji_enabled: false,
 			},
 		},
+		groupNames: ['日常']
 	},
 	{
 		id: 516,
