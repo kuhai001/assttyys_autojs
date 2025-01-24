572ef33590b16c114e29bb8a4a6b49b7ed08bc22
daxioanglaowang@qq.com <daxionglaowang@qq.com>
2025-01-24 13:24:41 +0800

    503,504标星

--- src/common/schemeList.ts
+++ src/common/schemeList.ts
@@ -1064,7 +1064,7 @@ const SchemeList: IScheme[] = [
 	{
 		id: 503,
 		'schemeName': '自定义定时任务_启动游戏_绘卷_个突_9退4_进攻',
-		'star': false,
+		'star': true,
 		'list': [0, 1, 2, 3, 502, 503, 993],
 		'config': {
 			'0': {
@@ -1081,7 +1081,7 @@ const SchemeList: IScheme[] = [
 	{
 		id: 504,
 		'schemeName': '自定义定时任务_启动游戏_绘卷_个人探索30次',
-		'star': false,
+		'star': true,
 		'list': [0, 1, 2, 3, 502, 503, 993],
 		'config': {
 			'0': {
