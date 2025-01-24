f9ea677673fa6064148ff2412fccc94e3c6cd3f2
daxioanglaowang@qq.com <daxionglaowang@qq.com>
2025-01-24 09:43:47 +0800

    完善绘卷功能

--- src/common/schemeList.ts
+++ src/common/schemeList.ts
@@ -1034,8 +1034,11 @@ const SchemeList: IScheme[] = [
 		id: 503,
 		'schemeName': '绘卷_返回庭院_个人30次探索',
 		'star': false,
-		'list': [3, 502, 503, 2],
+		'list': [0, 3, 502, 503, 2],
 		'config': {
+			'0': {
+				jspd_times_longtime_nodo: '3',
+			},
 			'503': {
 				'scheme_switch_enabled': true,
 				'next_scheme': '绘卷_个人探索30次'
@@ -1047,8 +1050,11 @@ const SchemeList: IScheme[] = [
 		id: 504,
 		'schemeName': '绘卷_返回庭院_突破_9退4_进攻',
 		'star': false,
-		'list': [3, 502, 503, 2],
+		'list': [0, 3, 502, 503, 2],
 		'config': {
+			'0': {
+				jspd_times_longtime_nodo: '3',
+			},
 			'503': {
 				'scheme_switch_enabled': true,
 				'next_scheme': '绘卷_个突_9退4_进攻'
@@ -1060,8 +1066,11 @@ const SchemeList: IScheme[] = [
 		id: 505,
 		'schemeName': '绘卷_定时任务_启动游戏_绘卷',
 		'star': false,
-		'list': [1, 2, 3, 503, 993],
+		'list': [0, 1, 2, 3, 503, 993],
 		'config': {
+			'0': {
+				jspd_times_longtime_nodo: '3',				
+			},
 			'503': { 'afterCountOper': '不进行任何操作' },
 			'993': {
 				'is_shutdown_the_game_before': true,
