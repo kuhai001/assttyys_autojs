95173c53b74aa65f98411d63d8d5c9e44cea2b7d
daxioanglaowang@qq.com <daxionglaowang@qq.com>
2025-05-19 09:23:15 +0800

    【调整方案】 突破找色相似度调整； 寄养改斗鱼

--- src/common/schemeList.ts
+++ src/common/schemeList.ts
@@ -983,6 +983,9 @@ const SchemeList: IScheme[] = [
 
 			}
 		},
+		commonConfig: {
+			multiColorSimilar: 96,
+		},
 		groupNames: ['绘卷']
 	},
 	{
@@ -1310,7 +1313,7 @@ const SchemeList: IScheme[] = [
 			},
 			'998': {
 				change_enchantment_switch: true,
-				change_enchantment_type: '太鼓',
+				change_enchantment_type: '斗鱼',
 			},
 		},
 	},
