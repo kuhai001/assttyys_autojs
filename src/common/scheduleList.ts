ee3c251a1c79bfe2a040b174c0c224abe3dca226
daxionglaowang <daxionglaowang@qq.com>
2026-07-20 14:41:16 +0800

    导入自制方案

--- src/common/scheduleList.ts
+++ src/common/scheduleList.ts
@@ -230,6 +230,141 @@ const ScheduleDefaultList: JobOptions[] = [
 			scheme: '对弈竞猜',
 		}
 	},
+	{
+		id: 501,
+		name: '自定义式神寄养',
+		desc: '自动续式神寄养，建议把执行时间提前5分钟，启动前需要退出游戏',
+		checked: true,
+		lastRunTime: null,
+		nextDate: null,
+		repeatMode: 2,
+		interval: '359',
+		nextOffset: '0,0',
+		level: '10',
+		config: {
+			scheme: '自定义定时任务-启动游戏-式神寄养',
+		}
+	},
+	{
+		id: 502,
+		name: '自定义每日签到与领取邮箱奖励',
+		desc: '每日签到与领取邮箱奖励',
+		checked: true,
+		lastRunTime: null,
+		nextDate: null,
+		repeatMode: 3,
+		interval: '0 20 17 * * *',
+		nextOffset: '0,0',
+		level: '5',
+		config: {
+			scheme: '自定义定时任务-启动游戏-每日签到与收取邮件',
+		}
+	},
+	{
+		id: 503,
+		name: '自定义地鬼',
+		desc: '自动地鬼',
+		checked: true,
+		lastRunTime: null,
+		nextDate: null,
+		repeatMode: 3,
+		interval: '0 45 18 * * *',
+		nextOffset: '0,0',
+		level: '6',
+		config: {
+			scheme: '自定义定时任务-启动游戏-地鬼',
+		}
+	},
+	{
+		id: 504,
+		name: '自定义金币妖怪',
+		desc: '金币妖怪',
+		checked: true,
+		lastRunTime: null,
+		nextDate: null,
+		repeatMode: 3,
+		interval: '0 45 17 * * *',
+		nextOffset: '0,0',
+		level: '6',
+		config: {
+			scheme: '自定义定时任务-启动游戏-金币妖怪',
+		}
+	},
+	{
+		id: 505,
+		name: '自定义寮突',
+		desc: '自动寮突',
+		checked: true,
+		lastRunTime: null,
+		nextDate: null,
+		repeatMode: 3,
+		interval: '0 0 14,21 * * *',
+		nextOffset: '0,0',
+		level: '1',
+		config: {
+			scheme: '自定义定时任务-启动游戏-寮突',
+		}
+	},
+	{
+		id: 506,
+		name: '自定义逢魔',
+		desc: '自动参加逢魔活动',
+		checked: true,
+		lastRunTime: null,
+		nextDate: null,
+		repeatMode: 3,
+		interval: '0 0 17 * * *',
+		nextOffset: '0,0',
+		level: '7',
+		config: {
+			scheme: '自定义定时任务-启动游戏-每日逢魔',
+		}
+	},
+	{
+		id: 506,
+		name: '自定义悬赏',
+		desc: '自动悬赏',
+		checked: true,
+		lastRunTime: null,
+		nextDate: null,
+		repeatMode: 3,
+		interval: '0 30 16,18,3 * * *',
+		nextOffset: '0,0',
+		level: '6',
+		config: {
+			scheme: '自定义定时任务-启动游戏-悬赏',
+		}
+	},
+	{
+		id: 507,
+		name: '自定义斗技',
+		desc: '周常-斗技',
+		checked: true,
+		lastRunTime: null,
+		nextDate: null,
+		repeatMode: 3,
+		interval: '0 30 14,21 * * 1,2,3,4,5',
+		nextOffset: '0,0',
+		level: '2',
+		config: {
+			scheme: '自定义定时任务-启动游戏-斗技',
+		}
+	},
+	{
+		id: 508,
+		name: '自定义绘卷',
+		desc: '自动绘卷',
+		checked: false,
+		lastRunTime: null,
+		nextDate: null,
+		repeatMode: 3,
+		interval: '0 30 0,6,12,19 * * 3,4,5,6,0',
+		nextOffset: '0,0',
+		level: '3',
+		config: {
+			scheme: '自定义定时任务_启动游戏_绘卷_个人探索30次',
+		}
+	},
 ];
 
 export default ScheduleDefaultList;
